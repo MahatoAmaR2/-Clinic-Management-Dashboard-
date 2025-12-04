89a77b7cbcdbac84af51d9da4977c8b98b57c524
MahatoAmaR2 <bauaa476@gmail.com>
2025-12-04 20:06:42 +0530

    fix import

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/Sidebar.jsx";
 import Navbar from "../components/Navbar";
 
 const Layout = () => {
